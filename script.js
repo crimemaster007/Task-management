95885ca09dea36cb2b48f938374027ba6874ea70
Tushar kumar singh <prachand9211@gmail.com>
2022-09-23 15:07:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let hamburgerBtn = document.getElementById("hamburger-btn");
 let hamburgerMenu = document.getElementById("hamburger");
 let sidebarClose = document.getElementById("sidebar-close");
 
+hamburgerMenu.style.display="none";
 function hamburgerClick(){
   let element = document.getElementById("hamburger");
   element.classList.add("showMenu");
   sidebarClose.style.display="block";
+  hamburgerMenu.style.display="block";
 }
 hamburgerBtn.addEventListener("click",hamburgerClick);
 
@@ -13,5 +15,6 @@ function crossClick(){
   let element = document.getElementById("hamburger");
   element.classList.remove("showMenu");
   sidebarClose.style.display="none";
+  hamburgerMenu.style.display="none";
 }
 sidebarClose.addEventListener("click",crossClick);
